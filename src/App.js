97c44c6c3331e1a98dc02837d9a9e7eff1cb61f7
Satyajit Sahoo <satyajit.happy@gmail.com>
2018-05-28 00:45:15 +0200

    Change initial code

--- src/App.js
+++ src/App.js
@@ -4,9 +4,49 @@ import * as React from 'react';
 import Editor, { type Annotation } from './Editor';
 import ESLint from './vendor/eslint.bundle';
 
-const code = `function x() {
-  console.log("Hello world!");
-}`;
+const code = `import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Constants } from 'expo';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-elements'; // Version can be specified in package.json
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>
+          Change code in the editor and watch it change on your phone!
+          Save to get a shareable url.
+        </Text>
+        <Card title="Local Modules">
+          <AssetExample />
+        </Card>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+});
+`;
 
 type State = {
   files: {
